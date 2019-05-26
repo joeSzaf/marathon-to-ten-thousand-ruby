c77a518f316fb99d03813645ec7193785899cda8
Joe Szafarowicz <joeszaf@gmail.com>
2019-05-26 14:28:43 -0400

    intial set up and update to foundation 6

---
+++ app/javascript/react/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Router, browserHistory, Route, IndexRoute } from 'react-router'
+
+
+export const App = (props) => {
+  return (
+    <div>
+      <p>Hello from the app!</p>
+    </div>
+
+
+  )
+}
+
+export default App
