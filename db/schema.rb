022a48bd5784267393aa59300d277b0cab7e010d
Joe Szaf <joeszaf@gmail.com>
2019-05-28 22:53:55 -0400

    create activities table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_26_171052) do
+ActiveRecord::Schema.define(version: 2019_05_28_214947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activities", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "type", null: false
+    t.date "date", null: false
+    t.float "duration", null: false
+    t.text "note"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_activities_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
